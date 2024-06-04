72b4d13adc178a4aa11e40c1b6fc7cf497148507
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2024-06-04 09:59:21 +0700

    feat/slot8-design

---
+++ app/src/main/java/com/infjz/prm392/slot8/Slot83.java
@@ -0,0 +1,21 @@
+package com.infjz.prm392.slot8;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.infjz.prm392.R;
+
+public class Slot83 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_slot83);
+    }
+}
